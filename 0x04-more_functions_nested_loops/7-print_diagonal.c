def5e8b24a5e03bb147aa672c18a5443958dd2b2
timkiarie <timmuigwa@gmail.com>
2023-03-24 12:20:31 -0700

    ./7-diagonals | cat -e

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
