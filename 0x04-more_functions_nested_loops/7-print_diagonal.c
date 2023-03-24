0275a67e1a59f84f600b547543ddd183994d3646
timkiarie <timmuigwa@gmail.com>
2023-03-24 12:49:06 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,10 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	
+
 	if (n <= 0)
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++);
 	{
 		for (j = 0; j < i; j++)
 		{
@@ -22,4 +22,5 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
+
 }
